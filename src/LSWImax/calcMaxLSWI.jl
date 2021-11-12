0d29e633c2a8630fc88c34b02f9dccd759140dc4
Russell Doughty <russell.doughty@ou.edu>
2021-11-12 13:12:33 -0600

    Initial commit

---
+++ src/LSWImax/calcMaxLSWI.jl
@@ -0,0 +1,14 @@
+###############################################################################
+#
+# Calculate Maximum LSWI for each year for Wscalar 
+#
+# Input are the 8-day 0.05-degree MCD43C4 LSWI nc files, which are annual.
+# Codes for producting LSWI are here: https://github.com/GeoCarb-OU/MCD43C4_VIs
+#
+# Output is one file for each year.
+#
+###############################################################################
+
+function max_lswi!(
+    file_list
+)
