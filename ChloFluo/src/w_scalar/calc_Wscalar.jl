5c488f4602b2e63150e397e739d1490900d81404
Russell Doughty <russell.doughty@ou.edu>
2021-11-14 10:39:37 -0600

    Working version

--- ChloFluo/src/w_scalar/calc_Wscalar.jl
+++ ChloFluo/src/w_scalar/calc_Wscalar.jl
@@ -12,40 +12,38 @@
 using NCDatasets
 using DataStructures
 using Statistics
-using Colors, Plots
+# using Colors, Plots
 
-lswi_nc    = "/mnt/g/ChloFluo/input/LSWI/1deg/MCD43C4.A.2020.LSWI.8-day.1deg.nc";
-lswimax_nc = "/mnt/g/ChloFluo/input/LSWImax/1deg/LSWImax.8-day.1deg.2020.nc";
-output_nc  = "/mnt/g/ChloFluo/input/w_scalar/1deg/LSWImax.8-day.1deg.2020.nc";
+lswi_nc    = "/mnt/g/ChloFluo/input/LSWI/1deg/MCD43C4.A.2018.LSWI.8-day.1deg.nc";
+lswimax_nc = "/mnt/g/ChloFluo/input/LSWImax/1deg/LSWImax.8-day.1deg.2018.nc";
+output_nc  = "/mnt/g/ChloFluo/input/wscalar/1deg/LSWImax.8-day.1deg.2018.nc";
 
-
-Wscalar =(10000+LSWI)*1.000/(10000+LSWImax)    # if lswi > 0, use the original function
-#WscalarCROP=numpy.where(LSWI<=0,LSWI+LSWImax,Wscalar) # if lswi<=0, adjust the wscalar using lswi+lswimax
-Wscalar=numpy.where(Wscalar>1,1,Wscalar)
-Wscalar=numpy.where(Wscalar<0,0,Wscalar)
-
-# Get maximum lswi
+# Calc wscalar for teach time step and return 3d array
 function calc_wscalar(lswi::String, lswi_max::String)
-    lswi     = Dataset(lswi_nc)["LSWI"]
-    lswi_max = Dataset(lswimax_nc)["LSWImax"]
+    
+    dates = Dataset(lswi)["time"]
+    dates = dates[:,:]
+    
+    lswi     = Dataset(lswi)["LSWI"]
+    lswi_max = Dataset(lswi_max)["LSWImax"]
     lswi     = reverse(mapslices(rotl90, lswi, dims = [1,2]), dims = 1)  # Rotate and reverse to correct lat/lon
     lswi_max = lswi_max[:,:]
     lswi     = Float32.(replace!(lswi, missing => NaN))
     lswi_max = Float32.(replace!(lswi_max, missing => NaN))
 
-    wscalar = (lswi[:,:,1] .+ 1.0) ./ (lswi_max[:,:] .+ 1.0)
-    wscalar[wscalar .> 1.0] .= 1.0
-    wscalar[wscalar .< 0.0] .= 0.0
+    wscalar_stack = zeros(Float32, size(lswi))
+    for i in 1:size(lswi)[3]
+        wscalar = (lswi[:,:,i] .+ 1.0) ./ (lswi_max[:,:] .+ 1.0)
+        wscalar[wscalar .> 1.0] .= 1.0
+        wscalar[wscalar .< 0.0] .= 0.0
+        wscalar_stack[:,:,i]     = wscalar
+    end
 
-
-
-
-
-    return data
+    return(wscalar_stack)
 end
 
 
-function save_nc(data, path)
+function save_nc(lswi, data, path)
     
     ds = Dataset(path, "c")
 
@@ -58,29 +56,30 @@ function save_nc(data, path)
     lat = collect(-90.0 + (res / 2.0) : res : 90.0 - (res / 2.0))
     lon = collect(-180.0 + (res / 2.0) : res : 180.0 - (res / 2.0))
 
-    defDim(ds,"lon", length(lon))
+    defDim(ds, "time", size(data)[3])
     defDim(ds,"lat", length(lat))
+    defDim(ds,"lon", length(lon))
 
-    dsLon    = defVar(ds, "lon" , Float32,("lon",), attrib = ["units" => "degrees_east", "long_name" => "Longitude"])
-    dsLat    = defVar(ds, "lat" , Float32,("lat",), attrib = ["units" => "degrees_north", "long_name" => "Latitude"])
-    dsLon[:] = lon
-    dsLat[:] = lat
+    dsTime    = defVar(ds, "time" ,Float32,("time",), attrib = ["units" => "days since 1970-01-01","long_name" => "Time (UTC), start of interval"])
+    dsLat     = defVar(ds, "lat" , Float32,("lat",), attrib = ["units" => "degrees_north", "long_name" => "Latitude"])
+    dsLon     = defVar(ds, "lon" , Float32,("lon",), attrib = ["units" => "degrees_east", "long_name" => "Longitude"])
+    dsTime[:] = Dataset(lswi)["time"][:,:] # Get dates from lswi time series
+    dsLat[:]  = lat
+    dsLon[:]  = lon
 
-    v = defVar(ds, "LSWImax", Float32, ("lat","lon"), deflatelevel = 4, fillvalue = -9999, attrib = ["units" => "Index", "long_name" => "LSWImax"])
+    v = defVar(ds, "wscalar", Float32, ("time", "lat", "lon"), deflatelevel = 4, fillvalue = -9999, attrib = ["units" => "Index", "long_name" => "Water Scalar"])
 
-    # Scale value does not work
-    # v = defVar(ds, "LSWImax", Float32, ("lat","lon"), deflatelevel = 4, fillvalue = -9999, attrib = OrderedDict(
-    #     "long_name"    => "LSWImax",
-    #     "units"        => "Index",
-    #     "scale_factor" => Float32(0.0001),
-    #     "add_offset"   => Float32(0.0)))
-
-    v[:,:] = data
+    # NC convention follows [z, y, x]
+    for t in 1:size(data)[3]
+         v[t,:,:] = data[:,:,t]
+    end
 
     close(ds)
 end
 
+wscalar = calc_wscalar(lswi_nc, lswimax_nc)
+save_nc(lswi_nc, wscalar, output_nc)
 
 # Take a look
-heatmap(lswi[:,:,20], clim = (-0.5, 0.5), bg = :white, color = :viridis)
-heatmap(wscalar, bg = :white, color = :viridis)
+# heatmap(lswi[:,:,20], clim = (-0.5, 0.5), bg = :white, color = :viridis)
+# heatmap(wscalar_stack[:,:,20], bg = :white, color = :viridis)
