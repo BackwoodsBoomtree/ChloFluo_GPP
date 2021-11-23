4ffc6bc3a270c3c6ed7c9c53f76ec47dfde6c9e3
Doughty <russell.doughty@ou.edu>
2021-11-23 16:11:27 -0600

    Add mask function

--- ChloFluo/src/landcover/clip_to_land.R
+++ ChloFluo/src/landcover/clip_to_land.R
@@ -6,9 +6,8 @@ coastlines <- "C:/Russell/R_Scripts/TROPOMI_2/mapping/GSHHS_shp/c/GSHHS_c_L1.shp
 r <- rast(toclip)
 v <- vect(coastlines)
 
-ext(r) <- c(-180, 180, -90, 90)
+test <- mask(r, v, touches = FALSE)
 
+plot(test[[1]], xlim = c(100,150), ylim = c(-10, 10))
+plot(v, add = TRUE)
 
-class(coastlines)
-extent(coastlines)
-crs(coastlines)
