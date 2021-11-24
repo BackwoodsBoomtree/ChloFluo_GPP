aa3fdca5d59b649fdafeee4ddc5c9295bbde45b9
Russell Doughty <russell.doughty@ou.edu>
2021-11-24 11:12:25 -0600

    Reorganize

--- ChloFluo/src/APARchl/calc_apar.jl
+++ ChloFluo/src/APARchl/calc_apar.jl
@@ -10,31 +10,33 @@
 using NCDatasets
 using Statistics
 include("../save/save_nc.jl")
-# using Colors, Plots
+using Colors, Plots
 
 sif_file    = "/mnt/g/ChloFluo/input/SIF/1deg/SIFqc.8day.1deg.CF80.2019.nc";
 yield_file  = "/mnt/g/ChloFluo/input/yield/1deg/yield.2019.8-day.1deg.nc";
 output_nc   = "/mnt/g/ChloFluo/input/APARchl/1deg/apar.2019.8-day.1deg.nc";
 year        = 2019;
 var_sname   = "aparchl";
-var_lname   = "APARchl";
+var_lname   = "Absorbed PAR by Chlorophyll";
 unit        = "μmol/m⁻²/day⁻¹";
 
 function calc_apar(sif, yield)
-    sif    = Dataset(sif)["sif743_qc"]
-    yield  = Dataset(yield)["SIFyield"]
+    sif    = Dataset(sif)["sif743_qc"][:,:,:]
+    yield  = Dataset(yield)["SIFyield"][:,:,:]
 
     # Arrange rasters dims to match
-    sif    = permutedims(sif, [2,3,1])
     sif    = replace!(sif, missing => NaN)
-    yield  = permutedims(yield, [2,3,1])
     yield  = replace!(yield, missing => NaN)
 
-    apar             = sif ./ yield;
-    apar             = replace!(apar, missing => NaN);
+    apar             = sif ./ yield
+    apar             = replace!(apar, missing => NaN)
     apar[apar .< 0] .= NaN;
+    apar             = permutedims(apar, [2,1,3])
 
     save_nc(apar, output_nc, year, var_sname, var_lname, unit)
 end
 
 calc_apar(sif_file, yield_file);
+
+# Take a look
+# heatmap(apar[:,:,23], bg = :white, color = :viridis)
--- ChloFluo/src/LUE/calc_LUE.jl
+++ ChloFluo/src/LUE/calc_LUE.jl
@@ -15,29 +15,38 @@
 # using GMT
 using NCDatasets
 using StatsBase
-using Colors, Plots
-include("save/save_nc.jl")
+# using Colors, Plots
+include("../save/save_nc.jl")
 
-# input_hdf  = "/mnt/g/ChloFluo/input/landcover/mcd12c1/MCD12C1.A2018001.006.2019200161458.hdf";
 c4_map     = "/mnt/g/ChloFluo/input/C3C4/ISLSCP/c4_percent_1d.nc";
-output_lue = "/mnt/g/ChloFluo/input/LUE/1deg/LUEmax.1deg.nc";
+output_nc = "/mnt/g/ChloFluo/input/LUE/1deg/LUEmax.1deg.nc";
+y           = 2019;
+var_sname   = "LUEmax";
+var_lname   = "Maximum Light Use Efficiency";
+unit        = "g C / mol";
 
 # Possible future use
+# input_hdf  = "/mnt/g/ChloFluo/input/landcover/mcd12c1/MCD12C1.A2018001.006.2019200161458.hdf";
 # LUT_LUEe = Dict([(0 => NaN), (1 => 0.78), (2 => 0.78), (3 => 0.78), (4 => 0.78), (5 => 0.78), (6 => 0.78), (7 => 0.78), (8 => 0.78), (9 => 0.78), (10 => 0.78), (11 => 0.78), (12 => 0.78), (13 => 0.78), (14 => 0.78), (15 => 0.78), (16 => 0.78)]);
 # full_path = input_hdf * "=gd?HDF4_EOS:EOS_GRID:\"" * input_hdf * "\":MOD12C1:Majority_Land_Cover_Type_1";
 # land = gmt("read -Tg " * full_path);
 # LUE_map = [LUT_LUEe[value] for value in land];
 
-c4_perc = Dataset(c4_map)["Band1"][:,:]
-c4_perc = rotr90(c4_perc)
-c4_perc = reverse!(c4_perc, dims = 2)
-c4_perc = replace!(c4_perc, missing => NaN)
-c4_perc = c4_perc / 100
+function calc_luemax(map)
 
-lue = (c4_perc .* 0.117) + ((1 .- c4_perc) .* 0.078)
+    c4_perc = Dataset(map)["Band1"][:,:]
+    c4_perc = rotr90(c4_perc)
+    c4_perc = reverse!(c4_perc, dims = 2)
+    c4_perc = replace!(c4_perc, missing => NaN)
+    c4_perc = c4_perc / 100
 
-save_nc(lue, output_lue, "LUEmax", "Maximum Light Use Efficiency");
+    lue = (c4_perc .* 0.117) + ((1 .- c4_perc) .* 0.078)
+
+    save_nc(lue, output_nc, y, var_sname, var_lname, unit)
+end
+
+calc_luemax(c4_map)
 
 # Take a look
-heatmap(lue, bg = :white, color = :viridis)
+# heatmap(lue, bg = :white, color = :viridis)
 
