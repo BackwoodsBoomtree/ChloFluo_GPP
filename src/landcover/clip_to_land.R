eb102948dfbcc86d20e2d2b87be2a85a4c41d685
Doughty <russell.doughty@ou.edu>
2021-11-30 11:46:58 -0600

    Working version

--- src/landcover/clip_to_land.R
+++ src/landcover/clip_to_land.R
@@ -1,13 +1,16 @@
 library(terra)
 
-toclip     <- "G:/ChloFluo/product/v01/1deg/ChloFluo.GPP.v01.1deg.CF80.2019.nc" # Main directory
+toclip     <- "G:/ChloFluo/product/v01/1deg/ChloFluo.GPP.v01.1deg.CF80.2019.nc"
 coastlines <- "C:/Russell/R_Scripts/TROPOMI_2/mapping/GSHHS_shp/c/GSHHS_c_L1.shp"
 
 r <- rast(toclip)
 v <- vect(coastlines)
 
-test <- mask(r, v, touches = FALSE)
+clipped <- mask(r, v, touches = FALSE)
 
-plot(test[[1]], xlim = c(100,150), ylim = c(-10, 10))
+plot(clipped[[1]], xlim = c(100,150), ylim = c(-10, 10))
 plot(v, add = TRUE)
 
+writeCDF(clipped, "G:/ChloFluo/product/v01/1deg/clipped/ChloFluo.GPP.v01.1deg.CF80.2019.clip.nc",
+         varname = "gpp", longname = "Gross Primary Production", unit = "g C/m-2/day-1",
+         missval = -9999, overwrite = TRUE)
